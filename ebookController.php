4e084e4698ec42cd868a934e294326e62e536925
mdfitriaiman <69753620+mdfitriaiman@users.noreply.github.com>
2020-12-09 21:58:14 +0800

    Create ebookController.php

---
+++ ebookController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ebookController extends Controller
+{
+    public function about() {
+        return view('subview/about');
+    }
+
+    public function list() {
+        $title1 = 'The Cat in the Hat';
+        $isbn1 = '978-0-7172-6059-1';
+        $bookCategory1 = 'Children's Literature';
+        $noPages1 = '61';
+
+        $title2 = 'The Lorax';
+        $isbn2 = '0-394-82337-0';
+        $bookCategory2 = 'Children's Literature';
+        $noPages2 = '64';
+
+        return view('subview/list', compact('title1', 'isbn1', 'bookCategory1', 'noPages1', 
+        'title2', 'isbn2', 'bookCategory2', 'noPages2'));
+    }
+}
