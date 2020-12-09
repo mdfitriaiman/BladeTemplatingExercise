687b0a370638b5cfe95e9cea02d0e0d3db4de82d
mdfitriaiman <69753620+mdfitriaiman@users.noreply.github.com>
2020-12-09 22:05:38 +0800

    Update about.blade.php

--- about.blade.php
+++ about.blade.php
@@ -6,8 +6,8 @@
 
 <h1>E-book sharing application</h1>
 
-<p>If you love books but are running low on shelf space, this application provide a wonderful alternative. 
-They’re conveniently portable, don’t take up space, and can be enjoyed from anywhere 
-(not to mention all the trees you’ll save each year).<p>
+<p>Book publishing is a nerve-wracking endeavor for every writer – both budding and established. While few have the marketing savvy required for self-publishing, choosing the right eBook hosting platform can significantly expedite the process. 
+
+Going the traditional route has its own benefits since every author wants to get their book published in print; into something that is tangible. However, one cannot underestimate the reach of digital platforms in today’s times.<p>
 
 @endsection
